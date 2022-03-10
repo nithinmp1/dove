4488c6f56921555315cc767674070b3e0a22ff8c
ksashokan <ashokanks1122@gmail.com>
2022-03-10 18:15:40 +0530

    second

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 var hbs = require('express-handlebars');
+var fileUpload = require('express-fileupload');
 
 var app = express();
 
@@ -27,9 +28,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(fileUpload())
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
