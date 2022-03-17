f6b3570b91b8274372c096f245c8ec0cacdbf241
ksashokan <ashokanks1122@gmail.com>
2022-03-17 15:40:00 +0530

    auth

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var userRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var hbs = require('express-handlebars');
 var fileUpload = require('express-fileupload');
+var session = require('express-session');
 var db = require('./config/connection')
 var app = express();
 
@@ -29,6 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
+app.use(session({secret:"key",cookie:{maxAge:600000}}))
 
 db.connect((err) =>{
   if(err) console.log("Connection Error"+err)
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var productHelper = require('../helpers/product-helpers');
+var userHelper = require('../helpers/user-helpers');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  let user = req.session.user;
+  console.log('fffffffffffffffffffffff',user);
   productHelper.getAllProducts().then((products) => {
     console.log(products);
-    res.render('user/view-products', { admin: false, products });
+    res.render('user/view-products', { admin: false, products, user });
   })
     
     // res.render('index', { products, admin: false });
@@ -18,6 +21,34 @@ router.get('/login', (req, res) => {
 
 router.get('/signup', (req, res) => {
   res.render('user/signup');
+})
+
+router.post('/signup', (req, res) => {
+  userHelper.doSignup(req.body).then((response) => {
+    console.log(response);
+    if(response) {
+      res.redirect('/login');
+    } else {
+      res.redirect('/signup')
+    }
+  })
+})
+
+router.post('/login', (req, res) => {
+  userHelper.doLogin(req.body).then((response) => {
+    if(response.status) {
+      req.session.loggedIn=true
+      req.session.user=response.user
+      res.redirect('/')
+    } else {
+      res.redirect('/login')
+    }
+  })
+})
+
+router.get('/logout', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
 })
 
 module.exports = router;
